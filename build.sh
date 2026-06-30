5da97acc8f80c082e7d83918b179ff7082a8c789
KADIRI-SANDEYYA <kadirisandeyya2003@gmail.com>
2026-06-30 17:10:54 +0530

    Deploy FoodExpress App

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+set -o errexit
 
 pip install -r requirements.txt
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-
